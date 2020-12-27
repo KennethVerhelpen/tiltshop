df3f6e7c1f96b0181c902661cf446c254cebadb0
Kenneth <kenneth.verhelpen@gmail.com>
2020-12-27 11:38:52 +0100

    Sitempa generator

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   images: {
     domains: ['tiltshop.co'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/sitemap-generator')
+    }
+    return config
+  }
 }
