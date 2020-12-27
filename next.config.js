673828055a10a2af0ac774840158557fb27e5a61
Kenneth <kenneth.verhelpen@gmail.com>
2020-12-27 13:54:05 +0100

    Globby removed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   images: {
     domains: ['tiltshop.co'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./utils/sitemap-generator')
-    }
-    return config
   }
 }
