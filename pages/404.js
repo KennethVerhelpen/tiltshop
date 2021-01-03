03f8c01e1fba4ad634ec29a49693bd150d37005f
Kenneth <kenneth.verhelpen@gmail.com>
2021-01-03 11:36:21 +0100

    Fallback false on article detailsé

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,19 @@
 import Link from 'next/link';
+import styled from '@emotion/styled';
+
 
 export default function Custom404() {
+
+  const Title = styled.h1`
+    font-size: 10rem;
+    @media only screen and (max-width: 599px) {
+      font-size: 5rem; 
+    }
+  `
+
   return (
-    <main style={{ background: 'black', color: 'white' }} className="layout-column layout-fill layout-align-center-center">
-      <h1 className="strong serif" style={{ fontSize: '10rem' }}>404</h1>
+    <main style={{ background: 'black', color: 'white' }} className="layout-column layout-fill layout-align-center-center text-center p-32">
+      <Title className="strong serif">404</Title>
       <h2 className="h5">Oops, something went wrong</h2>
       <Link href={'/'}>
         <a className="p mt-16 underline" aria-label="Back to home">Back to home</a>
--- pages/[category]/[medium]/[article].js
+++ pages/[category]/[medium]/[article].js
@@ -64,7 +64,7 @@ export async function getStaticPaths() {
 	
   return {
 		paths,
-		fallback: true,
+		fallback: false,
   }
 };
 
